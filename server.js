a821e23f94d8e88996eac6cca3e5d747f2444265
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2025-01-28 19:06:31 -0500

    added Post new Book /  ejs form

--- server.js
+++ server.js
@@ -12,12 +12,22 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
+app.use(express.urlencoded({ extended: false }));
 
 // ROUTES
 
 // New Book Route
 app.get("/books/new", (req, res) => {
   res.render("./books/new.ejs");
+});
+
+// Create Route
+app.post("/books", async (req, res) => {
+  //   console.log(req.body);
+
+  const newBook = await Book.create(req.body);
+  if (!newBook) return res.status(400);
+  res.status(201).redirect("./books/new");
 });
 
 // PORT CONNECTION
